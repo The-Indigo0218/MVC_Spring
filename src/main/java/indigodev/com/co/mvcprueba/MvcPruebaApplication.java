28f339de2e789e2454766c090149ca110b522476
MiguelDev <migopa0222@gmail.com>
2025-10-27 18:45:13 -0500

    Initial config

---
+++ src/main/java/indigodev/com/co/mvcprueba/MvcPruebaApplication.java
@@ -0,0 +1,13 @@
+package indigodev.com.co.mvcprueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcPruebaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcPruebaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/indigodev/com/co/mvcprueba/MvcPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package indigodev.com.co.mvcprueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcPruebaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
