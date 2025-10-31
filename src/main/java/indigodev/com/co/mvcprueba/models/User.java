6c20f655aec05dbf0bd3ccbc778e0c18b3a86983
MiguelDev <migopa0222@gmail.com>
2025-10-30 23:59:16 -0500

    POJO y view

---
+++ src/main/java/indigodev/com/co/mvcprueba/models/User.java
@@ -0,0 +1,40 @@
+package indigodev.com.co.mvcprueba.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String lastName;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String userName;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    public User(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    @PrePersist
+    public void prePersist(){
+        this.createdAt = LocalDateTime.now();
+    }
+}
