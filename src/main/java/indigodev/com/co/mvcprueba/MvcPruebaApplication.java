1ece148d0456600a0c9fe06625d9f6a5944c8fbe
MiguelDev <migopa0222@gmail.com>
2025-11-02 19:15:22 -0500

    enable auditory

--- src/main/java/indigodev/com/co/mvcprueba/MvcPruebaApplication.java
+++ src/main/java/indigodev/com/co/mvcprueba/MvcPruebaApplication.java
@@ -2,8 +2,10 @@ package indigodev.com.co.mvcprueba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MvcPruebaApplication {
 
     public static void main(String[] args) {
