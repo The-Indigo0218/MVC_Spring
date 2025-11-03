5e345d2171f68d3772305e2883c333b596607675
MiguelDev <migopa0222@gmail.com>
2025-11-03 00:58:03 -0500

    FT: Change callSuper = false to = true

--- src/main/java/indigodev/com/co/mvcprueba/models/User.java
+++ src/main/java/indigodev/com/co/mvcprueba/models/User.java
@@ -6,7 +6,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-@EqualsAndHashCode(callSuper = false, onlyExplicitlyIncluded = true)
+@EqualsAndHashCode(callSuper = true, onlyExplicitlyIncluded = true)
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
