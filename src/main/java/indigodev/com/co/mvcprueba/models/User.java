3359c9411bf94959c28476dd2a2d9ee165434e4a
MiguelDev <migopa0222@gmail.com>
2025-11-09 18:32:55 -0500

    Modified User model

--- src/main/java/indigodev/com/co/mvcprueba/models/User.java
+++ src/main/java/indigodev/com/co/mvcprueba/models/User.java
@@ -1,6 +1,8 @@
 package indigodev.com.co.mvcprueba.models;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
