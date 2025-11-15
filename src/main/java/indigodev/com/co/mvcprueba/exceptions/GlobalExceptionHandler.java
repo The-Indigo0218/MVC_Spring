db2bbdf287c82b6205393df402d5b91cd5ea7cf3
MiguelDev <migopa0222@gmail.com>
2025-11-14 19:59:51 -0500

    FT: return a modelAndView in the GlobalExceptionHandler

--- src/main/java/indigodev/com/co/mvcprueba/exceptions/GlobalExceptionHandler.java
+++ src/main/java/indigodev/com/co/mvcprueba/exceptions/GlobalExceptionHandler.java
@@ -5,21 +5,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleResourceNotFoundException(ResourceNotFoundException ex, Model model) {
-        model.addAttribute("errorMessage", ex.getMessage());
-        return "error-page";
+    public ModelAndView handleResourceNotFoundException(ResourceNotFoundException ex) {
+       ModelAndView view = new ModelAndView();
+       view.addObject("error", ex.getMessage());
+       view.setViewName("error-page");
+       return view;
     }
 
     @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) // HTTP 500
-    public String handleGeneralException(Exception ex, Model model) {
-        model.addAttribute("errorMessage", "Unexpected error: " + ex.getMessage());
-        return "error-page";
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ModelAndView handleGeneralException(Exception ex) {
+        ModelAndView view = new ModelAndView();
+        view.addObject("error", ex.getMessage());
+        view.setViewName("error-page");
+        return view;
     }
 }
